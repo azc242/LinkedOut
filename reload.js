964eed901c469b003855b631a7a8af337334dc3c
Alan Chen <azc242@nyu.edu>
2020-12-26 12:53:31 -0800

    get feed to load without showing actual feed

--- reload.js
+++ reload.js
@@ -1,5 +1,5 @@
 document.getElementById("global-nav").querySelector("a[href='/feed/']").addEventListener('click', handleClick, true);
 function handleClick() {
-  console.log("loiaded");
+  console.log("loaded");
   window.location.href = 'https://www.linkedin.com/feed';
 }
