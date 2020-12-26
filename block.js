0ca44015dc1f8c3df0d76da196521afe23ac4ea0
Alan Chen <azc242@nyu.edu>
2020-12-25 20:14:22 -0800

    get middle middle feed block to remove without shifting entire page left

--- block.js
+++ block.js
@@ -1,8 +1,15 @@
-removeElementsByClass("core-rail");
+// jshint esversion:6
+var elements = document.getElementsByClassName("core-rail");
 
-function removeElementsByClass(className){
-    var elements = document.getElementsByClassName(className);
-    while(elements.length > 0){
-        elements[0].parentNode.removeChild(elements[0]);
-    }
+for (var i = 0; i < elements.length; i++) {
+  elements.item(i).innerHTML = '';
 }
+
+// removeElementsByClass("core-rail");
+
+// function removeElementsByClass(className){
+//     var elements = document.getElementsByClassName(className);
+//     while(elements.length > 0){
+//         elements[0].parentNode.removeChild(elements[0]);
+//     }
+// }
