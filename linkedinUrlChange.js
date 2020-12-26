41d2feddce44c4a51d70480aad7e14c3f2d92f4e
Alan Chen <azc242@nyu.edu>
2020-12-26 11:08:43 -0800

    remove urlChange js file and restructure images

--- linkedinUrlChange.js
+++
@@ -1,9 +0,0 @@
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    // listen for messages sent from background.js
-    if (request.message === 'hello!') {
-      console.log(request.url); // new url is now in content scripts!
-    }
-});
-
-console.log("HELLO");
