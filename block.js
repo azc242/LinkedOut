a5b0f3a988ff4209f7e576b93e41baf6194eb708
Alan Chen <azc242@nyu.edu>
2020-12-25 19:37:57 -0800

    initial commit

---
+++ block.js
@@ -0,0 +1,8 @@
+removeElementsByClass("core-rail");
+
+function removeElementsByClass(className){
+    var elements = document.getElementsByClassName(className);
+    while(elements.length > 0){
+        elements[0].parentNode.removeChild(elements[0]);
+    }
+}
