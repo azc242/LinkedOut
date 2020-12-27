dfe4f893cd226e038f2d64af475ae8770d9abd75
Alan Chen <azc242@nyu.edu>
2020-12-27 13:14:39 -0800

    update documentation and clean up block.js

--- background.js
+++ background.js
@@ -1,11 +1,11 @@
 // jshint esversion:6
 
+// adds listener for when URL is updated on a tab
 chrome.tabs.onUpdated.addListener(function(activeInfo, sender, sendResponse) {
     chrome.tabs.query({currentWindow: true, active: true}, function(tabs){
-      const re = new RegExp('feed', 'gi');
+      const re = new RegExp('feed', 'gi'); // looks for linkedin.com/feed with case insensitive
       const matches = tabs[0].url.match(re);
-      console.log(matches);
-      console.log(tabs[0].url);
+      // sends message to block feed if there was a URL match
       if(matches) {
         chrome.tabs.sendMessage(tabs[0].id, {action: "block_feed"}, function(response) {});
       } else {
--- block.js
+++ block.js
@@ -1,45 +1,41 @@
 // jshint esversion:6
 
-// TRY TO MAKE EVERYTHING INSIDE THE CORE-RAIL HIDDEN
-// const DEL_SELECTOR = ".core-rail";
-// const mo = new MutationObserver(onMutation);
-
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     // listen for messages sent from background.js
     if (request.action === 'block_feed') {
-      observe(function() {
-        console.log("Observing started");
-      });
+      observe(); // start MutationObserver to listen for changes
+      // block the feed
       onMutation([{addedNodes: [document.documentElement]}], true);
-      mo.disconnect();
-      insertImage();
+      mo.disconnect(); // disconnects MO to avoid blocking in other webpages
+      insertImage(); // inserts cat
       return true;
     } else {
-      return true;
+      // don't block the feed
+      return true; // keep listener on
     }
 });
 
-console.log("block.js runs");
-
+// init classname matching string and MutationObserver
 const DEL_SELECTOR = ".core-rail";
 const mo = new MutationObserver(onMutation);
-// in case the content script was injected after the page is partially loaded
-// onMutation([{addedNodes: [document.documentElement]}], true);
-// observe();
 
+/*
+  This function uses the MutationObserver instance and removes all HTML elements
+  with matches to DEL_SELECTOR
+*/
 function onMutation(mutations, hideElements) {
-  console.log("mutation runs");
   const toRemove = [];
-  for (const {
-      addedNodes
-    } of mutations) {
+  // loops through entire HTML document's nodes
+  for (const { addedNodes } of mutations) {
+    // loops through nodes of nodes
     for (const n of addedNodes) {
+      // if the element has a tag name (it is an element such as img, a, div, etc),
+      // then check if it has an ID or class name that matches DEL_SELECTOR
       if (n.tagName) {
         if (n.matches(DEL_SELECTOR)) {
           toRemove.push(n.children);
         } else if (n.firstElementChild && n.querySelector(DEL_SELECTOR)) {
           var matches = n.querySelectorAll(DEL_SELECTOR);
-
           for (var i = 0; i < matches.length; i++) {
             toRemove.push(...matches[i].children);
           }
@@ -48,7 +44,7 @@ function onMutation(mutations, hideElements) {
     }
   }
   if (toRemove.length) {
-    mo.disconnect();
+    mo.disconnect(); // stop observing for changes
     for (const el of toRemove) {
       try {
         el.remove();
@@ -56,10 +52,13 @@ function onMutation(mutations, hideElements) {
         console.log(e);
       }
     }
-    // observe();
+    // observe(); // disabled to prevent continuous observing
   }
 }
 
+/*
+  Starts MutationObserver so that it listens for changes to the DOM
+*/
 function observe() {
   mo.observe(document, {
     subtree: true,
@@ -67,19 +66,16 @@ function observe() {
   });
 }
 
-const Http = new XMLHttpRequest();
+const Http = new XMLHttpRequest(); // makes GET requests to API
 const url = 'https://api.thecatapi.com/v1/images/search';
 Http.open("GET", url);
 Http.responseType = "json";
 Http.send();
 
-window.onload = function() {
-  console.log("inserting image");
-  // insertImage();
-};
-
+/*
+  This function inserts the fake post on LinkedIn's feed page with a cat image from the API
+*/
 function insertImage() {
-  console.log("loaded");
   const header1 = "<div id=\"ember63\" class=\"ember-view\"> <div data-id=\"urn:li:activity:6747916846474915841\" id=\"ember67\" class=\"relative ember-view\"> <div data-urn=\"urn:li:activity:6747916846474915841\" id=\"ember70\" class=\"feed-shared-update-v2 feed-shared-update-v2--minimal-padding full-height relative feed-shared-update-v2--e2e artdeco-card ember-view\"> <div <div=\"\" id=\"ember85\" class=\"display-flex feed-shared-actor display-flex ember-view\"> <a data-control-id=\"YwzeQP8sOyL91Wd9+jV5Lg==\" data-control-name=\"actor_container\" target=\"_self\" href=\"https://linkedin.com/in/alanzchen242/\" id=\"ember86\" class=\"feed-shared-actor__container-link relative display-flex flex-grow-1 app-aware-link ember-view\"> <div class=\"feed-shared-actor__container absolute\"></div><div class=\"feed-shared-actor__image relative\" data-control-id=\"YwzeQP8sOyL91Wd9+jV5Lg==\" data-control-name=\"actor_picture\"> <span class=\"js-feed-shared-actor__avatar\" data-entity-hovercard-id=\"urn:li:fs_miniProfile:ACoAACxMVqsBmH-bdKeLA8Lqbz5y3_iu4ZLsqWM\"> <div id=\"ember87\" class=\"feed-shared-actor__avatar ivm-image-view-model ember-view\"> <div id=\"ember88\" class=\"display-flex ivm-view-attr__img-wrapper ivm-view-attr__img-wrapper--use-img-tag ember-view\"> <div id=\"ember129\" class=\"presence-entity presence-entity--size-3 ember-view\"> <img title=\"Alan Chen\" src=\"https://media-exp1.licdn.com/dms/image/C5603AQGHHAloc-FtAQ/profile-displayphoto-shrink_200_200/0/1606436831730?e=1614211200&v=beta&t=ugdCMvCK2DwiN0fH-fPQS96enwPmEmCoCpJEvq64aqM\" loading=\"lazy\" id=\"ember130\" class=\"ivm-view-attr__img--centered EntityPhoto-circle-3 feed-shared-actor__avatar-image presence-entity__image EntityPhoto-circle-3 lazy-image ember-view\"> <div id=\"ember131\" class=\" presence-entity__indicator presence-entity__indicator--size-3 presence-indicator hidden presence-indicator--size-3 ember-view\"> <span class=\"visually-hidden\"> Status is offline </span></div></div></div></div></span> </div><div class=\"feed-shared-actor__meta relative\" data-control-id=\"YwzeQP8sOyL91Wd9+jV5Lg==\" data-control-name=\"actor\"> <span class=\"feed-shared-actor__title\"> <span class=\"feed-shared-actor__name t-14 t-bold hoverable-link-text t-black\" data-entity-hovercard-id=\"urn:li:fs_miniProfile:ACoAACxMVqsBmH-bdKeLA8Lqbz5y3_iu4ZLsqWM\"> <span dir=\"ltr\">Alan Chen</span> </span> <span class=\"feed-shared-actor__supplementary-actor-info t-14 t-normal ml1 t-black--light\"> • 2nd </span> </span> <span class=\"feed-shared-actor__description t-12 t-normal t-black--light\"> CS Student at NYU </span> <span class=\"feed-shared-actor__sub-description t-12 t-normal t-black--light\"> <span> <span aria-hidden=\"true\"> Now • <li-icon aria-hidden=\"true\" type=\"globe-icon\" class=\"v-align-bottom\" size=\"small\"> <svg xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 16 16\" data-supported-dps=\"16x16\" fill=\"currentColor\" class=\"mercado-match\" width=\"16\" height=\"16\" focusable=\"false\"> <path d=\"M8 1a7 7 0 107 7 7 7 0 00-7-7zM3 8a5 5 0 011-3l.55.55A1.5 1.5 0 015 6.62v1.07a.75.75 0 00.22.53l.56.56a.75.75 0 00.53.22H7v.69a.75.75 0 00.22.53l.56.56a.75.75 0 01.22.53V13a5 5 0 01-5-5zm6.24 4.83l2-2.46a.75.75 0 00.09-.8l-.58-1.16A.76.76 0 0010 8H7v-.19a.51.51 0 01.28-.45l.38-.19a.74.74 0 01.68 0L9 7.5l.38-.7a1 1 0 00.12-.48v-.85a.78.78 0 01.21-.53l1.07-1.09a5 5 0 01-1.54 9z\"> </path> </svg> </li-icon> </span> <span class=\"visually-hidden\">Now</span> </span> </span> </div></a> </div><div style=\"\" id=\"ember90\" class=\"feed-shared-update-v2__description-wrapper ember-view\"> <div tabindex=\"-1\" id=\"ember91\" class=\"feed-shared-inline-show-more-text feed-shared-update-v2__description feed-shared-inline-show-more-text--minimal-padding ember-view\"> <div dir=\"ltr\" id=\"ember92\" class=\"feed-shared-text relative feed-shared-update-v2__commentary ember-view\"> <span class=\"break-words\"> <span dir=\"ltr\"> Here's some pawsitivty for you today. Have a great day! </span> </span></div></div></div>";
 
   const header2 = "</div></div></div>";
@@ -93,7 +89,7 @@ function insertImage() {
 
     var coreRailHtml = document.getElementsByClassName("core-rail");
     coreRailHtml[0].innerHTML += imgEmbed;
-    console.log(coreRailHtml);
+    // console.log(coreRailHtml);
   } catch (e) {
     console.log(e);
   }
